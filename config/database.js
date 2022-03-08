8cc8dbb40c3a50c123e60c49144ba13b4265fe8d
QuocBao <quocbaovu15101996@gmail.com>
2022-03-08 11:06:37 +0700

    refactor: change database config

--- config/database.js
+++ config/database.js
@@ -9,3 +9,22 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    debug: false,
+  },
+});
