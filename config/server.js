34df5c64c4b91bc1acfcfefed7288f0bd6c8ec13
QuocBao <quocbaovu15101996@gmail.com>
2022-03-10 15:05:46 +0700

    refactor: config env

--- config/server.js
+++ config/server.js
@@ -6,10 +6,3 @@ module.exports = ({ env }) => ({
   },
 });
 
-// module.exports = ({ env }) => ({
-//   url: env("MY_HEROKU_URL"),
-//   proxy: true,
-//   app: {
-//     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
-//   },
-// });
